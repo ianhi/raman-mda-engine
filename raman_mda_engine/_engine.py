daad9cd61e5ba49f6e6c6730b497cee7443ec6f7
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2022-05-12 16:34:21 -0400

    add better fake images to engine

--- raman_mda_engine/_engine.py
+++ raman_mda_engine/_engine.py
@@ -1,8 +1,18 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
-from pymmcore_plus.mda import MDAEngine
+
+from functools import lru_cache
+from typing import TYPE_CHECKING, Tuple
+
+import numpy as np
 from loguru import logger
 from psygnal import Signal
+from pymmcore_plus.mda import MDAEngine
+from skimage.draw import disk
+from useq import MDAEvent
+
+if TYPE_CHECKING:
+    from pymmcore_plus import CMMCorePlus
+    from useq import MDASequence
 
 if TYPE_CHECKING:
     from pymmcore_plus import CMMCorePlus
@@ -14,14 +24,61 @@ class RamanEvents:
 
 
 class RamanEngine(MDAEngine):
-    def __init__(self, mmc: "CMMCorePlus" = None) -> None:
+    def __init__(
+        self,
+        mmc: "CMMCorePlus" = None,
+        img_shape=(512, 512),
+        step_size: float = 10,
+        radius: float = 30,
+        init_spread: float = 100,
+    ) -> None:
         super().__init__(mmc)
         self.raman_events = RamanEvents()
+        self._rng = np.random.default_rng()
+        self._img_shape = np.asarray(img_shape)
+        self._last_pos = {}
+        self._step_size = step_size
+        self._radius = radius
+        self._init_spread = init_spread
 
     def _sequence_axis_order(self, seq: MDASequence):
         event = next(seq.iter_events())
         event_axes = list(event.index.keys())
         return tuple(a for a in seq.axis_order if a in event_axes)
+
+    def _sequence_axis_order(self, seq: MDASequence):
+        event = next(seq.iter_events())
+        event_axes = list(event.index.keys())
+        return tuple(a for a in seq.axis_order if a in event_axes)
+
+    def _prepare_to_run(self, sequence: MDASequence):
+        self._axis_order = self._sequence_axis_order(sequence)
+        P = sequence.shape[self._axis_order.index("p")]
+        for p in range(P):
+            if p not in self._last_pos:
+                Np = self._rng.integers(low=3, high=10)
+                self._last_pos[p] = self._rng.normal(
+                    self._img_shape / 2, self._init_spread, (Np, 2)
+                )
+
+        # for convenience
+        self.out = np.zeros((*sequence.shape, *self._img_shape))
+        return super()._prepare_to_run(sequence)
+
+    # cache to avoid adavancing the implicit time step when working through
+    # the different channels
+    @lru_cache
+    def _gen_img(self, p, t=None):
+        img = np.zeros(self._img_shape, dtype=np.uint8)
+        pos = self._last_pos[p]
+        pos += self._rng.normal(0, self._step_size, pos.shape)
+        self._last_pos[p] = pos
+        for p in pos:
+            img[disk(p, self._radius, shape=self._img_shape)] = 1
+        return img
+
+    def _event_to_index(self, event: MDAEvent) -> Tuple[int, ...]:
+        return tuple(event.index[a] for a in self._axis_order)
 
     def run(self, sequence: MDASequence) -> None:
         """
@@ -63,8 +120,10 @@ class RamanEngine(MDAEngine):
                     self.raman_events.collectRamanSpectra.emit(p, t)
                     logger.info(f"collecting raman: {p=}, {t=}, {z=}")
 
-            self._mmc.snapImage()
-            img = self._mmc.getImage()
+            img = self._gen_img(event.index.get("p", None), event.index.get("t", None))
+            self.out[self._event_to_index(event)] = img
+            # self._mmc.snapImage()
+            # img = self._mmc.getImage()
 
             self._events.frameReady.emit(img, event)
         self._finish_run(sequence)
