bea15b3a2a571c57b0a78dc61de5925c8c8819bc
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2022-11-08 08:45:59 -0500

    circle

--- raman_mda_engine/aiming/transformers.py
+++ raman_mda_engine/aiming/transformers.py
@@ -8,6 +8,7 @@ __all__ = [
     "Identity",
     "Crosshair",
     "Square",
+    "Circle",
 ]
 
 
@@ -105,7 +106,7 @@ class Square(Transformer):
         edge_length : float, optional
             Length of the edge. If *None* use self.spacing
         N_points : int, optional
-            Number of points to have per edge. If *None* use self.spacing
+            Number of points to have per edge. If *None* use self.N_points
 
         Returns
         -------
@@ -117,5 +118,66 @@ class Square(Transformer):
         edge = np.linspace(-edge_length / 2, edge_length / 2, N_points)
         X, Y = np.meshgrid(edge, edge)
         stacked = np.vstack([X.ravel(), Y.ravel()])
+
+        return (xy + stacked.T[:, None, :]).reshape(-1, 2)
+
+
+class Circle(Transformer):
+    def __init__(self, radius: float, N_on_radius: int):
+        """
+        Parameters
+        ----------
+        radius : float
+            Radius of the circle in relative coordinates
+        N_on_radius : int
+            Number of points to have from the center point (maybe inclusive)
+            to the edge (inclusive)
+        """
+        super().__init__()
+        self._radius = radius
+        self._N_on_radius = N_on_radius
+
+    @property
+    def radius(self) -> float:
+        return self._radius
+
+    @radius.setter
+    def radius(self, val: float):
+        self._radius = val
+
+    @property
+    def N_on_radius(self) -> int:
+        return self._N_on_radius
+
+    @N_on_radius.setter
+    def N_on_radius(self, val: int):
+        if not isinstance(val, numbers.Integral) or val <= 0:
+            raise TypeError("N_on_radius must be a positive integer")
+
+    def transform(self, xy, radius: float = None, N_on_radius: int = None):
+        """
+        Make a circle centered on the points.
+
+        Parameters
+        ----------
+        xy : (N, 2) arraylike
+        radius : float, optional
+            Radius of the circle in relative coordinates. If *None*, use self.radius
+        N_on_radius : int, optional
+            Number of points to have from the center point
+            to the edge (inclusive). If *None* use self.N_on_radius
+
+        Returns
+        -------
+        xy (N_new, 2)
+            In a circle shape.
+        """
+        radius = radius or self.radius
+        N = N_on_radius or self.N_on_radius
+
+        diam = np.linspace(-radius, radius, N)
+        X, Y = np.meshgrid(diam, diam)
+        idx = np.sqrt(X**2 + Y**2) <= radius
+        stacked = np.vstack([X[idx].ravel(), Y[idx].ravel()])
 
         return (xy + stacked.T[:, None, :]).reshape(-1, 2)
