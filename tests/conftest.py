1c3377e3f86025a687d803c3625869e61ac16faf
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2023-02-21 13:10:17 -0500

    add basic tests of mda + raman

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import pytest
 from pymmcore_plus import CMMCorePlus
 
+from raman_mda_engine import RamanEngine, fakeAcquirer
+from raman_mda_engine.aiming import SimpleGridSource
+
 
 @pytest.fixture
 def core() -> CMMCorePlus:
@@ -10,3 +13,11 @@ def core() -> CMMCorePlus:
     if len(mmc.getLoadedDevices()) < 2:
         mmc.loadSystemConfiguration(str(Path(__file__).parent / "test-config.cfg"))
     return mmc
+
+
+@pytest.fixture
+def engine(core: CMMCorePlus) -> RamanEngine:
+    engine = RamanEngine(spectra_collector=fakeAcquirer())
+    engine.aiming_sources.append(SimpleGridSource(5, 5))
+    core.register_mda_engine(engine)
+    return engine
